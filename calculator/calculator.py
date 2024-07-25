27a824e46b8a21747d3e70563dbb7aff0d187d39
YooSJ <qpxjfdl@naver.com>
2024-07-25 17:35:59 +0900

    Python

---
+++ calculator/calculator.py
@@ -0,0 +1,12 @@
+﻿num1 = int(input("첫번째 숫자를 입력하세요."))
+calc = input("연산자를 입력하세요('+', '-', '*', '/')")
+num2 = int(input("두번째 숫자를 입력하세요."))
+
+if(calc == "+"):
+    print("결과값 :", num1+num2)
+if(calc == "-"):
+    print("결과값 :", num1-num2)
+if(calc == "*"):
+    print("결과값 :", num1*num2)
+if(calc == "/"):
+    print("결과값 :", num1/num2)
