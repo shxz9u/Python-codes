0cfab9bea008da19496a8154ff34e659e6f7d787
YooSJ <qpxjfdl@naver.com>
2024-08-30 22:47:28 +0900

    Python

--- Subject Average/Subject Average/Subject_Average.py
+++ Subject Average/Subject Average/Subject_Average.py
@@ -1,1 +1,20 @@
+print("Korean score")
+kor = int(input())
+if kor > 100:
+    print("Wrong Answer")
+    exit()
+print("Math score")
+math = int(input())
+if math > 100:
+    print("Wrong Answer")
+    exit()
+print("English socre")
+eng = int(input())
+if eng > 100:
+    print("Wrong Answer")
+    exit()
 
+total = kor+math+eng
+avg = total/3
+
+print("average :" , avg)
